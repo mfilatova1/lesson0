ee7f4347006ceee93c67bd245a4da027b531eec9
mfilatova1 <filatovam435@gmail.com>
2024-06-08 12:27:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+a = "world"
+print(len(a))
+first = 5
+second = 7
+summa = (first + second)
+diff = (first - second)
+print(summa)
+print(diff)
+first = 6
+second = 3
+third = 9
+mean = (first + second + third) / 3
+print(mean)
+first_string = "вторник"
+second_string = "понедельник"
+print(second_string, first_string)
+a = 2
+b = 3
+c = 4
+f = (a * b) + (a * c)
+print((f ** 3) / 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
